978d5fd0c00a253dd1c9c5121c4dea86cd1ad539
Garrincha <goepfert.benoit@gmail.com>
2017-07-24 01:24:43 +0200

    test datatable

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
+    <datatable :columns="table_columns" :data="table_rows" filterable paginate></datatable>
     <router-view></router-view>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,42 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.component('datatable', require('vuejs-datatable'))
+
+Vue.component('edit-button', {
+  template: `
+        <button class="btn btn-xs btn-primary" @click="goToUpdatePage">Edit</button>
+    `,
+  props: ['row'],
+  methods: {
+    goToUpdatePage: function () {
+      window.location = '/contact/' + this.row.id + '/update'
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  data: {
+    table_columns: [
+      { label: 'First Name', field: 'first_name' },
+      { label: 'Last Name', field: 'last_name' },
+      { label: 'Email', field: 'email' },
+      {
+        label: 'age',
+        callback: function (row) {
+          var ageDiff = Date.now() - (new Date(row.birthdate).getTime())
+          var ageDate = new Date(ageDiff)
+
+          return Math.abs(ageDate.getUTCFullYear() - 1970)
+        }
+      },
+      { label: '', component: 'edit-button' }
+    ],
+    table_rows: []
+  }
 })
